237ae1281182ea23dad89b636aa16b5852899edb
zcr <zcr@zcr>
2017-03-27 15:33:46 +0800

    第一次提交vue代码到本地仓库

---
+++ sell/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+import header from 'components/header/header'
+
+export default {
+  name: 'app',
+  components: {
+    'v-header': header
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
